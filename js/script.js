bb0b3dada4d06f968f57e0f2b3d75195b6087bad
Pedro Pondian <overdadeiropedrojoao53@gmail.com>
2025-09-10 22:32:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const navbar = document.getElementById('navbar');
+const navbar_parte_cima = document.getElementById('navbar_parte_cima');
 
+window.addEventListener("scroll", () => {
+    //console.log(scrollY)
+
+    if(scrollY > 800){
+        navbar.style.background = "rgba(0, 0, 0, 0.3)";
+    }else{
+        navbar.style.background = "transparent";
+    }
+
+    if(scrollY > 2050){
+        navbar_parte_cima.style.transform = "translateY(-120px)";
+        navbar_parte_cima.style.height = "10px";
+    }else{
+        navbar_parte_cima.style.transform = "unset";
+        navbar_parte_cima.style.height = "unset";
+    }
+
+});
