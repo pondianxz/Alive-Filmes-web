0b7fab4e1ae6f29ab0cf3e67cbd52ba52d643136
Pedro Pondian <overdadeiropedrojoao53@gmail.com>
2025-09-14 15:44:06 -0300

    Add files via upload

--- Controller/conexao.php
+++ Controller/conexao.php
@@ -9,8 +9,8 @@ try{
     $conexao = mysqli_connect($bd_servidor, $bd_user, $bd_senha, $bd_nome);
 }catch(mysqli_sql_exception){
     echo'
-        <h1 style="color: red; font-size: 80px; transform: translateY(100px);">Não foi possível conectar ao servidor</h1>
-        ';
+    <h1 style="color: red; font-size: 80px; transform: translateY(100px); text-align: center;">Não foi possível conectar ao servidor</h1>
+    ';
 }
 
 ?>
