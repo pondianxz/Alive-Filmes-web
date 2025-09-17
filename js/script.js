179ae9adbac1615d637dc8447ac4a1f91fe33803
Pedro Pondian <overdadeiropedrojoao53@gmail.com>
2025-09-17 19:26:23 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-const navbar = document.getElementById('navbar');
+const navbar            = document.getElementById('navbar');
 const navbar_parte_cima = document.getElementById('navbar_parte_cima');
 
+
+paginaAtual = window.location.pathname;
+
 window.addEventListener("scroll", () => {
-    //console.log(scrollY)
+    switch (true) {
+        // caso o user esteja na pagina index
+        case (paginaAtual === ""):
+        case (paginaAtual.includes("index.php")):
+            if(scrollY < 700){
+                navbar.style.background = "transparent";
+            }
+            else if(scrollY >= 700 && scrollY < 1650){
+                navbar.style.background = "rgba(0, 0, 0, 0.3)";
+            }
+            else if(scrollY >= 1650){
+                navbar_parte_cima.style.transform = "translateY(-120px)";
+                navbar_parte_cima.style.height = "10px";
+            } 
+            else{
+                navbar_parte_cima.style.transform = "unset";
+                navbar_parte_cima.style.height = "unset";
+            }
+            break;
 
-    if(scrollY > 700){
-        navbar.style.background = "rgba(0, 0, 0, 0.3)";
-    }else{
-        navbar.style.background = "transparent";
+
+        // caso o user esteja em algum arquivo da pasta de "sessoes", ou na pagina de contato
+        case (paginaAtual.includes("sessoes/")):
+        case (paginaAtual.includes("contato.php")):
+            navbar_parte_cima.style.transform = "translateY(-120px)";
+            navbar_parte_cima.style.height = "10px";
+            break;
     }
+})
 
-    if(scrollY > 2050){
-        navbar_parte_cima.style.transform = "translateY(-120px)";
-        navbar_parte_cima.style.height = "10px";
-    }else{
-        navbar_parte_cima.style.transform = "unset";
-        navbar_parte_cima.style.height = "unset";
-    }
-
-});
